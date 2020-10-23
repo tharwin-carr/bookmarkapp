0a5e34427ca7d8b538d8da3520c41ad282b7c19a
Tharwin <tharwin.carr08@gmail.com>
2020-10-22 22:52:30 -0700

    trying to deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+    API_ENDPOINT: `postgres://fxfkpzugwbtytv:7292f19aadda83b74bdd449c05e7b75faaf38b69494312f648d8ec466f0d9315@ec2-52-203-165-126.compute-1.amazonaws.com:5432/db8tqvcc7tijv4`,
     API_KEY: process.env.REACT_APP_API_KEY
   }
