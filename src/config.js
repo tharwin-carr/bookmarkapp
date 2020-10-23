fc7f430d0a740b21360e3dd834cb6ea73ac233c8
Tharwin <tharwin.carr08@gmail.com>
2020-10-22 23:06:07 -0700

    fixed config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `postgres://fxfkpzugwbtytv:7292f19aadda83b74bdd449c05e7b75faaf38b69494312f648d8ec466f0d9315@ec2-52-203-165-126.compute-1.amazonaws.com:5432/db8tqvcc7tijv4`,
+    API_ENDPOINT: `postgresql-flexible-54048.herokuapp.com`,
     API_KEY: process.env.REACT_APP_API_KEY
   }
